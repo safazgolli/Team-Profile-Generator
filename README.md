fe721807312bb827ae6652da6f4bc6f3c269510b
Safa Zgolli <fifi.zgolli89@gmail.com>
2022-10-12 23:21:02 -0400

    finish the readme file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Team-Profile-Generator
+
+## Description:
+application that  generate a webpage that displays  team's basic info, SO the user  can have quick access to their emails and GitHub profiles.
+
+* a command-line application that accepts user input
+* If  I am prompted for my team members and their information, an HTML file is generated that displays a nicely formatted team roster based on user input.
+* If I click on an email address in the HTML, my default email program opens and populates the TO field of the email with the address.
+* If I click on the GitHub username, that GitHub profile opens in a new tab.
+* If I start the application, I am prompted to enter the team manager’s name, employee ID, email address, and office number.
+* If I enter the team manager’s name, employee ID, email address, and office number, I am presented with a menu with the option to add an engineer or an intern or to finish building my team.
+* If I select the engineer option, I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu.
+* If I select the intern option, I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu.
+* If I decide to finish building my team, I exit the application, and the HTML is generated.
+
+### Walkthrough Video:
+ 
+ https://drive.google.com/file/d/1yzepoSx6HTz-m6APXlyPFggUxw6pW2JL/view
+
+ ### Pictures :
+ The following image shows a mock-up of the generated HTML’s appearance and functionality:
+
+
+ ![Alt screenshot1](./images/screenshot1.png)
+
+ ![Alt screenshot2](./images/screenshot2.png)
+
+
+  
--- index.js
+++ index.js
@@ -117,8 +117,8 @@ function cardHtml(member) {
             <h5 class="card-header">${name}<br /><br />Engineer</h5>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}"> ${email}</a></li>
+                <li class="list-group-item">GitHub:<a href = "https://github.com/${gitHub}">  ${gitHub} </a></li>
             </ul>
             </div>
         </div>`;
@@ -129,7 +129,7 @@ function cardHtml(member) {
             <h5 class="card-header text-bg-success">${name}<br /><br />Intern</h5>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Email Address:<a href="mailto:${email}"> ${email}</a></li>
                 <li class="list-group-item">School: ${school}</li>
             </ul>
             </div>
@@ -141,7 +141,7 @@ function cardHtml(member) {
             <h5 class="card-header text-bg-success">${name}<br /><br />Manager</h5>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Email Address: <a href="mailto:${email}"> ${email}</a></li>
                 <li class="list-group-item">Office Phone: ${officePhone}</li>
             </ul>
             </div>
